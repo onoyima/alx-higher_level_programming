3f6141db0bf03a5d73b5e5624f3b16aec1cac643
onoyima <clintonfaze@outlook.com>
2023-11-28 04:32:38 -0800

    comit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n < 0:
+    if n < 0 or n >= len(str):
         return str
-    else:
-        str = str[0:n] + str[n+1:]
-    return str
 
+    result = ""
+    for i in range(len(str)):
+        if i != n:
+            result += str[i]
+
+    return result
+
+# Test cases
+if __name__ == "__main__":
+    print(remove_char_at("Best School", 3))
+    print(remove_char_at("Chicago", 2))
+    print(remove_char_at("C is fun!", 0))
+    print(remove_char_at("School", 10))
+    print(remove_char_at("Python", -2))

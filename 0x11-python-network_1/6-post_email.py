5f392f30593d4d79112ba79ab02c70edf4dd9341
onoyima <clintonfaze@outlook.com>
2024-03-28 14:20:00 -0700

    COmmitting the 0x11-python project

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This script takes a URL and an email address as input, sends a POST request
+to the URL with the email as a parameter, and displays the body of the response.
+"""
+
 import requests
 import sys
 
@@ -9,4 +14,3 @@ if __name__ == "__main__":
     response = requests.post(url, data=payload)
     print("Your email is:", email)
     print(response.text)
-
